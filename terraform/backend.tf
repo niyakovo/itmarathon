c9e6259b5d6670e2d6ae9fb7870b5420060ec231
niyakovo <60444569+niyakovo@users.noreply.github.com>
2025-10-30 21:18:34 +0200

    Change S3 bucket name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "terraform-tfstate-igigkgjlkn"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
